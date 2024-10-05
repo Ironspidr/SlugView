050725dc20ff82e33dc6588c67fa489be1b218f4
etvlad76 <etvgarci@ucsc.edu>
2024-10-05 12:31:55 -0700

    main-replace

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 import * as THREE from 'three'
 import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { shader } from 'three/examples/jsm/nodes/Nodes.js';
 let camera, controls, scene, renderer;
+
+scene = new THREE.Scene();
+
+renderer = new THREE.WebGLRenderer( { antialias: true } )
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setAnimationLoop(animate)
+document.body.appendChild(renderer.domElement)
+
+camera = new THREE.PerspectiveCamera(60, window.innerWidth /window.innerHeight, 1 , 10000)
+camera.position.set(400,50,0)
+
+//Controls
+controls = new OrbitControls(camera, renderer.domElement)
+controls.listenToKeyEvents(window)
+controls.enableDamping = true
+controls.dampingFactor = 0.05
+
+controls.screenSpacePanning = false
+controls.minDistance = 100
+controls.maxDistance = 500
+
+controls.maxPolarAngle = Math.PI / 2
+
+//Objects
+
+//earth
+const Earfgeometry = new THREE.SphereGeometry(4,22,10)
+const Earfmaterial = new THREE.MeshBasicMaterial({color:0x0000FF})
+const shpereMesh = new THREE.Mesh(Earfgeometry,Earfmaterial)
+scene.add(shpereMesh)
+
+//stars
+const geometry = new THREE.SphereGeometry(1,22,10)
+const material = new THREE.MeshBasicMaterial({color:0xFFFFFF})
+
+for ( let i = 0; i < 500; i ++ ) {
+
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = Math.random() * 1600 - 800;
+    mesh.position.y = Math.random() * 1600 - 800;
+    mesh.position.z = Math.random() * 1600 - 800;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add( mesh );
+
+}
+
+//satALPH
+const satGeo = new THREE.BoxGeometry(2,2,2)
+const satMat = new THREE.MeshNormalMaterial({flatShading: true})
+const satellite = new THREE.Mesh(satGeo, satMat)
+scene.add(satellite)
 
 tick();
 
@@ -14,8 +68,8 @@ function tick() {
     renderer.setAnimationLoop(animate)
     document.body.appendChild(renderer.domElement)
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth /window.innerHeight, 1 , 1000)
-    camera.position.set(400,200,0)
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth /window.innerHeight, 1 , 10000)
+    camera.position.set(400,50,0)
 
     //Controls
     controls = new OrbitControls(camera, renderer.domElement)
@@ -30,11 +84,14 @@ function tick() {
     controls.maxPolarAngle = Math.PI / 2
 
     //Objects
+
+    //earth
     const Earfgeometry = new THREE.SphereGeometry(4,22,10)
     const Earfmaterial = new THREE.MeshBasicMaterial({color:0x0000FF})
     const shpereMesh = new THREE.Mesh(Earfgeometry,Earfmaterial)
     scene.add(shpereMesh)
 
+    //stars
     const geometry = new THREE.SphereGeometry(1,22,10)
     const material = new THREE.MeshBasicMaterial({color:0xFFFFFF})
 
@@ -49,6 +106,13 @@ function tick() {
         scene.add( mesh );
 
     }
+
+    //satALPH
+    const satGeo = new THREE.BoxGeometry(2,2,2)
+    const satMat = new THREE.MeshNormalMaterial({flatShading: true})
+    const satellite = new THREE.Mesh(satGeo, satMat)
+    scene.add(satellite)
+
     //lights
     const dirLight1 = new THREE.DirectionalLight(0xffffff, 3)
     dirLight1.position.set(1,1,1)
@@ -73,11 +137,39 @@ function windowResize() {
 }
 
 function animate() {
+    //time tracking
+    // const clock = new THREE.Clock()
+    // const delta = clock.getDelta()
+    // const elapsed = clock.elapsedTime
+
+    // //earth Pos
+    // shpereMesh.position.x = Math.sin(elapsed/2) * 3
+    // shpereMesh.position.z = Math.cos(elapsed/2) * 3
     controls.update()
 
     render()
 }
 
+// function animate()
+// {
+//     //time tracking
+//     var delta = clock.getDelta();
+//     var elapsed = clock.elapsedTime;
+    
+//     //sphere position
+//     sphere.position.x = Math.sin(elapsed/2) * 3;
+//     sphere.position.z = Math.cos(elapsed/2) * 3;
+    
+//     //satellite
+//     satellite.position.x =  sphere.position.x + Math.sin(elapsed*2) * 2;
+//     satellite.position.z = sphere.position.z + Math.cos(elapsed*2) * 2;
+//     satellite.rotation.x += 0.4 * delta;
+//     satellite.rotation.y += 0.2 * delta;
+    
+//     requestAnimationFrame( animate );
+//     controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+//     renderer.render( scene, camera );
+//}
 function render() {
     renderer.render(scene,camera)
 }
